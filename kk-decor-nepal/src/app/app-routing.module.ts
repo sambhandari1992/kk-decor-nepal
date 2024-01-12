6901abf2f03586ec3c4a90aba10504e4ede9d4e3
sbhandari1992 <sambhandari2021@yahoo.com>
2024-01-12 13:39:33 -0500

    changing css

--- kk-decor-nepal/src/app/app-routing.module.ts
+++ kk-decor-nepal/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+// app-routing.module.ts
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  // Add more routes for products, contact, etc.
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
